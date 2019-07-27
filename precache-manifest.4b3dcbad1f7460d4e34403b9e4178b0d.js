178e1506fa11473f08bcf5b558880678d3f2d898
unknown <abinet.tafa@gmail.com>
2019-07-27 13:07:00 -0700

    Updates

---
+++ precache-manifest.4b3dcbad1f7460d4e34403b9e4178b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcc3e4123ddf6a4cdee14ac4dcc9758",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "bb7bd5ea860728bc896a",
+    "url": "/ReactApp/static/css/main.e034ae08.chunk.css"
+  },
+  {
+    "revision": "d9f02bb93e4292a40391",
+    "url": "/ReactApp/static/js/2.e1a7675d.chunk.js"
+  },
+  {
+    "revision": "bb7bd5ea860728bc896a",
+    "url": "/ReactApp/static/js/main.6de8b897.chunk.js"
+  },
+  {
+    "revision": "8fa6fbb6c0e8399fe15f",
+    "url": "/ReactApp/static/js/runtime~main.eb98a27a.js"
+  }
+]);
